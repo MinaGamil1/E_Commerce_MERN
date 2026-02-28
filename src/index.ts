015147e9edcf51d9c0a69d01fb1a24d95044dea9
Mina Gamil <minagamil905@gmail.com>
2026-02-28 17:17:21 +0200

    Product Feature (CRUD)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRouter";
+import ProductRoute from "./routes/ProductRoute";
+import { seedInitialProducts } from "./services/ProductService";
 
 const app = express();
 const port = 3001;
@@ -10,7 +12,12 @@ mongoose
   .then(() => console.log("mongo connect"))
   .catch((err: any)=>console.log("faild to connect",err));
 
+  //seed the products to the database
+
+  seedInitialProducts();
+
 app.use('/user', userRoute);
+app.use('/products', ProductRoute);
 
   app.listen(port ,()=>
     {
---
+++ src/services/ProductService.ts
@@ -0,0 +1,21 @@
+import { get } from "mongoose";
+import ProductModel from "../models/ProductModel";
+
+export const getAllProducts = async () => {
+    return await ProductModel.find();
+};
+export const seedInitialProducts = async () => {
+    const Products =[
+        {
+            title: 'Dell Laptop',
+            price: 15000,
+            stock: 5,
+            image: '../imgs/dell-laptop.jpg'
+        },
+    ];
+
+    const existingProducts = await getAllProducts();
+    if (existingProducts.length === 0) {
+        await ProductModel.insertMany(Products);
+    };
+};
