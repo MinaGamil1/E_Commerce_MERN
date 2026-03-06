6b7f039f71b43d4e39c8cf0bd2867afc41346a31
Mina Gamil <minagamil905@gmail.com>
2026-03-06 03:12:50 +0200

    Implement update item functionality in cart service and route

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -3,13 +3,11 @@ import ProductModel from "../models/ProductModel";
 interface CreateCartForUser {
   userId: string;
 }
-
 const creatCartForUser = async ({ userId }: CreateCartForUser) => {
   const cart = await CartModel.create({ userId, totalAmount: 0 });
   await cart.save();
   return cart;
 };
-
 interface GetActiveCartForUser {
   userId: string;
 }
@@ -49,6 +47,41 @@ export const addItemToCart = async ({
   cart.items.push({ product: productId, unitPrice: product.price, quantity });
   cart.totalAmount += product.price * quantity;
   const updatedCart = await cart.save();
+  console.log("cart", cart);
 
   return { data: updatedCart, statusCode: 201 };
+};
+
+interface updateItemToCart {
+  userId: string;
+  productId: any;
+  quantity: number;
+}
+export const updateItemInCart = async ({
+  productId,
+  quantity,
+  userId,
+}: updateItemToCart) => {
+  const cart = await getActiveCartForUser({ userId });  
+  const existsInCart = cart.items.find((p) => p.product.toString() === productId);
+  if (!existsInCart) {
+    return { data: "Item does not exist in cart", statusCode: 400 };
+  } 
+  const product = await ProductModel.findById(productId);
+  if (!product) {
+    return { data: "Product not found", statusCode: 400 };
+  }
+  if (product.stock < quantity) {
+    return { data: "low stock for item", statusCode: 400 };
+  }
+  const otherCartItems = cart.items.filter((p) => p.product.toString()!== productId);
+  console.log("otherCartItems", otherCartItems);
+  let total = otherCartItems.reduce((sum,product) => {sum +=product.quantity * product.unitPrice  ;
+  return sum;
+  }, 0);
+  existsInCart.quantity = quantity;
+  total += existsInCart.quantity * existsInCart.unitPrice;
+  cart.totalAmount = total;
+  const updatedCart = await cart.save();
+  return { data: updatedCart, statusCode: 200 };
 };
